8056a8c561d82be7c788637904c45f686b447fc0
duanjunkai <duanjunkai_2013@163.com>
2024-05-03 20:25:31 +0800

    ONE完成

--- core-rest-api/src/main/java/com/djk/core/mq/ConsumerPull.java
+++ core-rest-api/src/main/java/com/djk/core/mq/ConsumerPull.java
@@ -130,6 +130,7 @@ public class ConsumerPull implements CommandLineRunner
                                                         .filter(item -> item.getStatus() == Constant.CRAWL_STATUS.RUNNING.ordinal())
                                                         .collect(Collectors.toList());
                                                 long offsetTime = System.currentTimeMillis() - startTime;
+                                                //相同的爬取请求前后2次需要间隔1分钟
                                                 if ((null == mergeList || mergeList.isEmpty()) && offsetTime > 1000 * 60) {
                                                     canCrawl = true;
                                                 }
