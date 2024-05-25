c78065ac1c0e192d4db1e7cb69c0ae2d311ff58a
xinzhi <duanjunkaisky@163.com>
2024-05-25 18:04:37 +0800

    time gmt

--- win-tool/src/main/java/com/djk/core/scheduling/CommandLine.java
+++ win-tool/src/main/java/com/djk/core/scheduling/CommandLine.java
@@ -15,48 +15,40 @@ import java.util.List;
  * @date 2024/05/25
  */
 @Component
-public class CommandLine
-{
-    public static final String CHROME_PATH = "C:\\Users\\Administrator\\AppData\\Local\\fortchrome\\Application\\";
+public class CommandLine {
+    public static final String CHROME_PATH = "C:\\Program Files\\Google\\Chrome\\Application";
     public static final String CHROME_PLUGIN_PATH = "D:\\chrome_plugin";
     public static final String CHROME_STARTUP_TIME = "05:00:00";
-    public static final String CHROME_SHUTDOWN_TIME = "21:00:00";
+    public static final String CHROME_SHUTDOWN_TIME = "17:34:00";
+
+    public static boolean hasShutDown = false;
+    public static boolean hasStartup = false;
 
     /**
      *
      */
     @Scheduled(cron = "0/5 * * * * ?")
-    private void checkChrome() throws Exception
-    {
+    private void checkChrome() throws Exception {
         long currentTimeMillis = System.currentTimeMillis();
         SimpleDateFormat sdfDay = new SimpleDateFormat("yyyy-MM-dd");
         String format = sdfDay.format(new Date());
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date shutDownDate = sdf.parse(format + " " + CHROME_SHUTDOWN_TIME);
-        String line;
         String target = "chrome.exe";
-        if (currentTimeMillis > shutDownDate.getTime()) {
-            //shutDown
-            try {
-                boolean processRunning = isProcessRunning(target);
-                if (processRunning) {
-                    Process taskkillProcess = Runtime.getRuntime().exec("taskkill /IM " + target + " /F");
-                    BufferedReader taskkillReader = new BufferedReader(new InputStreamReader(taskkillProcess.getInputStream()));
-                    while ((line = taskkillReader.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                    taskkillReader.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        if (currentTimeMillis > shutDownDate.getTime() && !hasShutDown) {
+            killTask(target);
+            hasShutDown = true;
+            hasStartup = false;
         }
         Date startUpDate = sdf.parse(format + " " + CHROME_STARTUP_TIME);
-        if (currentTimeMillis > startUpDate.getTime()) {
+        if (currentTimeMillis > startUpDate.getTime() && !hasStartup) {
+            hasStartup = true;
+            hasShutDown = false;
             //startUp
             try {
                 List<String> urlList = new ArrayList<>();
                 try {
+                    FileUtils.forceMkdir(new File(CHROME_PLUGIN_PATH));
                     BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(CHROME_PLUGIN_PATH + "\\config.txt")));
                     String urlLine = null;
                     while ((urlLine = br.readLine()) != null) {
@@ -70,24 +62,13 @@ public class CommandLine
                     return;
                 }
 
-                boolean processRunning = isProcessRunning(target);
-                if (!processRunning) {
-                    File directory = new File(CHROME_PLUGIN_PATH + "\\temp");
-                    if (directory.exists()) {
-                        try {
-                            FileUtils.deleteDirectory(directory);
-                            FileUtils.forceMkdir(new File(CHROME_PLUGIN_PATH + "\\temp"));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    for (int i = 0; i < urlList.size(); i++) {
-                        String url = urlList.get(i);
-                        Process process = Runtime.getRuntime().exec(
-                                CHROME_PATH + "\\chrome.exe --remote-allow-file-access-from-files --user-data-dir=\\\"D:\\\\chrome_plugin\\\\temp\\\" --start-maximized");
-                        process.getOutputStream().write(("cmd /c start chrome " + url).getBytes());
-                        process.getOutputStream().flush();
-                    }
+                killTask(target);
+                for (int i = 0; i < urlList.size(); i++) {
+                    String url = urlList.get(i);
+                    Process process = Runtime.getRuntime().exec(
+                            CHROME_PATH + "\\chrome.exe --remote-allow-file-access-from-files --user-data-dir=\\\"D:\\\\chrome_plugin\\\\temp\\\" --start-maximized");
+                    process.getOutputStream().write(("cmd /c start chrome " + url).getBytes());
+                    process.getOutputStream().flush();
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -95,8 +76,25 @@ public class CommandLine
         }
     }
 
-    public static boolean isProcessRunning(String processName)
-    {
+    private void killTask(String target) {
+        String line;
+        //shutDown
+        try {
+            boolean processRunning = isProcessRunning(target);
+            if (processRunning) {
+                Process taskkillProcess = Runtime.getRuntime().exec("taskkill /IM " + target + " /F");
+                BufferedReader taskkillReader = new BufferedReader(new InputStreamReader(taskkillProcess.getInputStream()));
+                while ((line = taskkillReader.readLine()) != null) {
+                    System.out.println(line);
+                }
+                taskkillReader.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean isProcessRunning(String processName) {
         try {
             String taskLineCommand = "tasklist.exe";
             Process process = new ProcessBuilder(taskLineCommand).start();
