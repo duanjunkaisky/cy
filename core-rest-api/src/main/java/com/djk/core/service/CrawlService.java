8c33a60ef46fc5e091f70d0db103876c89c28bfb
duanjunkai <duanjunkai_2013@163.com>
2024-06-02 20:51:09 +0800

    清除历史数据时机

--- core-rest-api/src/main/java/com/djk/core/service/CrawlService.java
+++ core-rest-api/src/main/java/com/djk/core/service/CrawlService.java
@@ -11,17 +11,65 @@ import com.djk.core.vo.QueryRouteVo;
 public interface CrawlService
 {
 
+    /**
+     * 爬取数据
+     *
+     * @param shippingCompany
+     * @param fromPort
+     * @param toPort
+     * @param queryRouteVo
+     * @return {@link String}
+     * @throws Exception
+     */
     String queryData(BaseShippingCompany shippingCompany, BasePort fromPort, BasePort toPort, QueryRouteVo queryRouteVo) throws Exception;
 
+    /**
+     * 获取起始港
+     *
+     * @param queryRouteVo
+     * @return {@link BasePort}
+     */
     BasePort getFromPort(QueryRouteVo queryRouteVo);
 
+    /**
+     * 获取到达港
+     *
+     * @param queryRouteVo
+     * @return {@link BasePort}
+     */
     BasePort getToPort(QueryRouteVo queryRouteVo);
 
+    /**
+     * 获取船司
+     *
+     * @param hostCode
+     * @return {@link BaseShippingCompany}
+     */
     BaseShippingCompany getShipCompany(String hostCode);
 
+    /**
+     * 增加爬取日志
+     *
+     * @param addDataId
+     * @param businessName
+     * @param stepName
+     * @param msg
+     * @param queryRouteVo
+     */
     void addLog(Boolean addDataId, String businessName, String stepName, String msg, QueryRouteVo queryRouteVo);
 
+    /**
+     * 绑定ip
+     *
+     * @param queryRouteVo
+     */
     void setTokenIp(QueryRouteVo queryRouteVo);
 
+    /**
+     * 标记需要删除的数据
+     *
+     * @param queryRouteVo
+     * @param shipId
+     */
     void flagDelData(QueryRouteVo queryRouteVo, Long shipId);
 }
