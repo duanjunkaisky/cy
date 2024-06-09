700f18c07b76ec036c215887f7021ca139e126a3
duanjunkai <duanjunkai_2013@163.com>
2024-06-09 09:11:34 +0800

    tls代理请求

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,7 @@
 注意:
 
+github-token: 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
+
 1、base_port要配置港口信息才能爬取，港口的 port_code要与调用传过来的一致，
     比如 传过来的 departurePortEn = port_code 并且 status = 0
     msk_code = msk网站前端可查的英文名，比如 岳阳->YUE YANG   上海->SHANGHAI
@@ -7,3 +9,24 @@
 2、船司表 要配置，比如 en_abbreviation = MSK
 3、需要给爬虫服务的时间做一个同步，不能时区不同
 4、fee_item需要增加  6:基础航运费
+
+
+
+
+
+
+
+//浏览器出现debuger循环，导致无法调试，把一下代码在console执行一下就可以了
+
+Function.prototype.__constructor_back = Function.prototype.constructor;
+Function.prototype.constructor = function() {
+    if(arguments && typeof arguments[0]==='string'){
+        //alert("new function: "+ arguments[0]);
+        if("debugger" === arguments[0]){
+            //arguments[0]="console.log(\"anti debugger\");";
+            //arguments[0]=";";
+            return
+        }
+    }
+   return Function.prototype.__constructor_back.apply(this,arguments);
+}
--- core-rest-api/src/test/java/com/djk/core/AppTest.java
+++ core-rest-api/src/test/java/com/djk/core/AppTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.Base64;
 import java.util.Date;
 
 
@@ -15,8 +16,15 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue() throws UnsupportedEncodingException
     {
-        String s = "https://synconhub.coscoshipping.com/spot?showInvExistOnly=true&showPromotionExistOnly=false&startDate=2024-05-24%2000%3A00&boundType=O&porCityName=%E4%B8%8A%E6%B5%B7%2C%E4%B8%AD%E5%9B%BD&porCityId=738872886232873&fndCityName=Constantza%2CRomania&fndCityId=738874496843885&page=1";
-        String decode = URLDecoder.decode(s, "utf-8");
-        System.out.println(decode);
+//        String s = "https://synconhub.coscoshipping.com/spot?showInvExistOnly=true&showPromotionExistOnly=false&startDate=2024-05-24%2000%3A00&boundType=O&porCityName=%E4%B8%8A%E6%B5%B7%2C%E4%B8%AD%E5%9B%BD&porCityId=738872886232873&fndCityName=Constantza%2CRomania&fndCityId=738874496843885&page=1";
+//        String decode = URLDecoder.decode(s, "utf-8");
+//        System.out.println(decode);
+        String s1 = "cGxpZWtqV01nQlBjUGFPaFFxamVWVTk2UTNKSVZOUjFYdEpLQkx1S2dpV2JKWWdHN21vZUV2VkRxdkY4R1ZYSkFnTlhweTBJcFI1QVEvSmNjVGY2dzRKOUdOcEhPcE5MUEs2bUQwcmYra2NacXJ6ajY2Qy9QTGZrbU5Obk1UNEJhY05tWGZVakE1emRHZ3V6TGU3REdLc2FlSGtNa0xyb0NTU1pQTElueDBYWkltYjZnWUJwQ0daQ1pTRE15OWR6ZWE=";
+        String s2 = new String(Base64.getDecoder().decode(s1.getBytes()));
+        System.out.println(s2);
+
+        String s3 = "BBCA5DDCD3ADB03E76CA9EDF537BE7BB~YAAQjeBb2tN6seKPAQAAdtgP7hifZn+Lsy0n8LsGoh41qWZ3F6CdvP1aefdLVgqbOTW2tEJtgZV3MgKY0+0H4DUFzL5+pybbVoilbRqmMlldkiQyX9+5gL8tJeemRSVJ2LZi7tgjhK4ynds2cEV0t2rujBSkSaNKzlcrV+kaL1MjE0L5RYmVh+jeNB9Ez+MbncF9DttlRqunnPzKOP7AYV0CkQ7+EAPABVV4nj+9HtVL7A/Dp7lbb9rzVVrn52hCbvqUuCQQNloLboTkZr37NB9fGnSVpt2AdH68yf7SFbZjjz53Xy+S7rJKYVZTwoCWEFv2apO/qapeOlMgKPWQ45nWZwb4Npx7OgMihcOmNNzrkhAoAIlhDrmlhvH243OEJY9QBobz1lCbTjuLo0AFThaF4W8WtL4f9IEWy75b2lG6vi5v4X+Ugna9qmjNNG0nCcoc/RsKvt46Cg==~3225665~4599873";
+        String s = Base64.getEncoder().encodeToString(s3.getBytes());
+        System.out.println(s);
     }
 }
